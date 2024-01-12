3155e321e7302465e9d93597b5df1143edc1f16b
AndreyMoraesFriedel <AndreyMoraesFriedel@gmail.com>
2024-01-12 17:11:46 -0300

    Aula de Condições JS

---
+++ CursoJS/Condições/exx01/main.js
@@ -0,0 +1,6 @@
+var vel = 30;
+console.log(`A velocidade do seu carro é de ${vel}Km/h`);
+if(vel>=60){
+    console.log(`Você foi MULTADO por excesso de velocidade!`);
+}
+console.log(`Dirija sempre com cinto de segurança!`);
---
+++ CursoJS/Condições/exx02/main.js
@@ -0,0 +1,6 @@
+var pais = `Brazil`
+if(pais==`Brazil`){
+    console.log(`Sextou! Partiu Neyday!`);
+}else{
+    console.log(`Hermano tu eres gringo???`);
+}
